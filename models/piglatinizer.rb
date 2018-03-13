b0c9a5c8a29be1038a3b7993f65a682baf016fad
Chely Ho <adept_inept@yahoo.com>
2018-03-13 20:43:35 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -2,6 +2,7 @@ class PigLatinizer
   attr_accessor :phrase
 
   def piglatinize(phrase)
+    pig = ''
     phrase.split.map do |word|
       while word.chars.first.match(/[^aeiouAEIOU]/)
         pig = word.chars.push(word.chars.first)
