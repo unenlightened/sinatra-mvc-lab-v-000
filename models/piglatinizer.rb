1c455c2a24b719219f4083988c1dd05c75c25c84
Chely Ho <adept_inept@yahoo.com>
2018-03-13 21:02:57 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -4,18 +4,21 @@ class PigLatinizer
   def piglatinize(phrase)
 
     phrase.split.map do |word|
-      word.chars.first.match(/[^aeiouAEIOU]/) ? word.insert(-1, 'ay') : word.insert(-1, 'way')
+      first_character = word.chars.first
+      
+      while word.chars.first.match(/[^aeiouAEIOU]/)
+        pig = word.chars.push(word.chars.first)
+        pig.shift
+        word = pig
+      end
+      
+      first_character.match(/[^aeiouAEIOU]/) ? word.insert(-1, 'ay') : word.insert(-1, 'way')
 
       word.downcase == word ? word : word.capitalize
     end.join(' ')
 
 
-    phrase.split.map do |word|
-      while word.chars.first.match(/[^aeiouAEIOU]/)
-        pig = word.chars.push(word.chars.first)
-        pig.shift
-        word = pig
-      end 
+    #
     #
     #   word.chars.first.match(/[^aeiouAEIOU]/) ? pig.insert(-1, 'ay') : pig.insert(-1, 'way')
     #
