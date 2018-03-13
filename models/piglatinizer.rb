c6060f45e1e8d45a795b0a965e35382133a9db9d
Chely Ho <adept_inept@yahoo.com>
2018-03-13 21:00:33 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -10,11 +10,12 @@ class PigLatinizer
     end.join(' ')
 
 
-    # phrase.split.map do |word|
-    #   while word.chars.first.match(/[^aeiouAEIOU]/)
-    #     pig = word.chars.push(word.chars.first)
-    #     pig.shift
-    #   end
+    phrase.split.map do |word|
+      while word.chars.first.match(/[^aeiouAEIOU]/)
+        pig = word.chars.push(word.chars.first)
+        pig.shift
+        word = pig
+      end 
     #
     #   word.chars.first.match(/[^aeiouAEIOU]/) ? pig.insert(-1, 'ay') : pig.insert(-1, 'way')
     #
